0a90a3ff516c768e74ffc808cec7f8d5b15304a5
Mikael Pajari <mikael.pajari@live.se>
2020-09-16 07:17:48 +0200

    räkna ränta per år

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _9nk2ny
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            float accbalance = 50000;
+            float goals = 100000;
+            float rate = 3;
+
+            
+            for(int years = 1; accbalance <= goals; years++ )
+            {
+                
+                if(accbalance <= goals)
+                {
+                    accbalance = accbalance * (1 + rate/100);
+                    Console.WriteLine($"på {years} år så har du {accbalance} på kontot.");
+                }
+            }
+            
+            
+
+            
+
+        }
+    }
+}
